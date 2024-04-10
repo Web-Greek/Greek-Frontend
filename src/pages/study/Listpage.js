7cb9143e1c1e0685765cf19a8d8ed2e5e08ffebd
Minminminwoo <alsdnsms07@naver.com>
2024-04-10 15:24:32 +0900

    Update Listpage.js
    listpage 디자인 변

--- src/pages/study/Listpage.js
+++ src/pages/study/Listpage.js
@@ -1,73 +1,110 @@
-import * as React from "react";
-import {Card,CardContent,CardMedia} from "@mui/material";
-import CssBaseline from "@mui/material/CssBaseline";
-import {Grid,Typography,Container} from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Link } from "react-router-dom";
+import * as React from 'react';
+import { Card, CardContent, CardMedia, TextField, Button } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Grid, Typography, Container, Box } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import EditIcon from '@mui/icons-material/Edit';
+import { Link } from 'react-router-dom';
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cards = [1, 2, 3, 4]; // Adjust the number of cards as per your 2x2 layout
 
-// TODO remove, this demo shouldn't need to reset the theme.
+
 const defaultTheme = createTheme();
 
+
 function Listpage() {
+
+  const backgroundImage = 'https://miro.medium.com/v2/resize:fit:1200/1*6Jp3vJWe7VFlFHZ9WhSJng.jpeg';
+  //const backgroundImage = 'https://i0.wp.com/www.oxbridgeacademy.edu.za/blog/wp-content/uploads/2015/07/CN63QSUO8C.jpg?resize=1280%2C640&ssl=1';
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline>
-        <Container sx={{ mt: 3 }} maxWidth="md">
-          <main>
-            {/* Hero unit */}
-            <Container sx={{ py: 4 }} maxWidth="md">
-              {/* End hero unit */}
-              <Grid container spacing={4}>
-                {cards.map((card) => (
-                  <Grid item key={card} xs={12} sm={6} md={4}>
-                    <Card
-                      sx={{
-                        height: "100%",
-                        display: "flex",
-                        flexDirection: "column",
-                      }}
-                    >
-                      <Link
-                        to={`/study/detail/${card}`}
-                        style={{ textDecoration: "none" }}
-                      >
-                        {/* 포스터 클릭 시 DetailPage로 이동하는 링크 */}
-                        <CardMedia
-                          component="div"
-                          sx={{
-                            // 16:9
-                            pt: "56.25%",
-                          }}
-                          image="https://source.unsplash.com/random?wallpapers"
-                        />
-                        <CardContent
-                          sx={{
-                            flexGrow: 1,
-                            display: "flex",
-                            justifyContent: "space-between",
-                          }}
-                        >
-                          <div>
-                            <Typography
-                              gutterBottom
-                              variant="h5"
-                              component="h2"
-                            >
-                              제목 {card} {/* 각 카드에 다른 제목을 표시 */}
-                            </Typography>
-                            <Typography>User ID {card}</Typography>
-                          </div>
-                        </CardContent>
-                      </Link>
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
-            </Container>
-          </main>
-        </Container>
+          
+          <Box sx={{ position: 'relative', mt: 3, mb: 3 }}>
+            {/* Background image with transparency */}
+            <Box
+              sx={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                opacity: 0.4, //이미지 투명도
+                zIndex: -1,
+              }}
+            />
+            {/* Search bar container */}
+              <Container maxWidth="md">
+                <Box sx={{ display: 'flex', justifyContent: 'center', paddingTop: '20vh', paddingBottom: '5vh' }}>
+                  <TextField id="search" label="검색어를 입력해주세요" variant="filled" color='success' 
+                    sx={{ 
+                      mr: 2, 
+                      width: 600, 
+                      '& .MuiFilledInput-root': {
+                        backgroundColor: 'white',
+                        '&:hover': {
+                          backgroundColor: 'white',
+                        },
+                        '&.Mui-focused': {
+                          backgroundColor: 'white',
+                        },
+                      },
+                      
+                      }} />
+                  <Button variant="contained" color="success">
+                    Go!
+                  </Button>
+                </Box>
+              </Container>
+          </Box>
+
+        
+          {/* Posts cards container */}
+          <Container maxWidth="md" sx={{ pt: 10 }}>
+            <Grid container spacing={8}>
+              {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} sx={{ px: 4 }}>
+                  <Card
+                    sx={{
+                      height: '100%',
+                      display: 'flex',
+                      flexDirection: 'column',
+                    }}
+                  >
+                    <Link to={`/study/detail/${card}`} style={{ textDecoration: 'none' }}>
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image="https://source.unsplash.com/random?wallpapers"
+                        alt={`Content ${card}`}
+                      />
+                      <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography gutterBottom variant="h5" component="h2" sx={{ color: 'black' }}>
+                          제목 {card}
+                        </Typography>
+                        <Typography sx={{ color: 'black' }}>User ID {card}</Typography>
+                      </CardContent>
+                    </Link>
+                  </Card>
+                </Grid>
+              ))}
+
+            </Grid>
+
+            
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 4, mr: 4  }}>
+              <Button variant="contained" endIcon={<EditIcon />} sx={{ backgroundColor: '#000', color: '#fff', '&:hover': { backgroundColor: 'rgba(0, 0, 0, 0.8)' } }}>
+                작성하기
+              </Button>
+            </Box>
+
+          </Container>
+
+          
       </CssBaseline>
     </ThemeProvider>
   );
